db71cd68326fc9c6b33ecf55c32e88ca70d4dde1
Ahmed Gamal <ahmedgemy2212@gmail.com>
2022-05-14 11:19:27 +0200

    DSU

--- DSU.cpp
+++ DSU.cpp
@@ -1,161 +1,30 @@
-// Those who cannot remember the past are
-// condemned to repeat it (use DP -_-)
-// - George Santayana
 
-#include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
+struct dsu{
+    vector<int> p, rank;
 
-using namespace std;
-using namespace __gnu_pbds;
+    explicit dsu(int size){
+        p.resize(size + 1);
+        rank.resize(size + 1);
 
-#define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(), v.rend()
-#define ll long long
-#define ull unsigned long long
-#define MOD 1000000007
-#define PI 3.14159265
-#define ceil(a, b) (((a) / (b)) + ((a) % (b) ? 1 : 0))
-#define imin INT_MIN
-#define imax INT_MAX
-#define llmax LLONG_MAX
-#define llmin LLONG_MIN
-#define inf 2000000000
-#define nl '\n'
-#define ppcnt __builtin_popcount
-#define ppcntll __builtin_popcountll
-#define clz __builtin_clz
-#define clzll __builtin_clzll
-#define ctz __builtin_ctz
-#define ctzll __builtin_ctzll
-#define modulo(a, b, mod) ((((a) % (mod)) * ((b) % (mod))) % (mod))
-#define cnte(v, x) count(all(v), (x))
-#define mine(v) min_element(all(v))
-#define maxe(v) max_element(all(v))
-#define updmin(a, b) a = min(a, b)
-#define updmax(a, b) a = max(a, b)
-#define findmod(x, m) x = ((x) % (m) + (m)) % m
-#define getmod(x, m) ((x) % (m) + (m)) % (m)
-#define debug(x) cout << "x: " << (x) << nl;
-#define debug2(x, y) cout << "x: " << (x) << " y: " << y << nl;
-#define ordered_set tree<ll, null_type, less<>, rb_tree_tag, tree_order_statistics_node_update>
-#define ordered_map tree<int, int, less<>, rb_tree_tag, tree_order_statistics_node_update>
-
-//vector<int> dx = {0, 0, 1, -1, 1, 1, -1, -1}, dy = {1, -1, 0, 0, 1, -1, 1, -1};
-//vector<int> dx = {0, 0, 1, -1}, dy = {1, -1, 0, 0};
-
-template<typename T = int> istream& operator>>(istream& in, vector<pair<int, int>>& v){
-    for (auto& [x, y] : v) in >> x >> y;
-    return in;
-}
-
-template<typename T = int> istream& operator>>(istream& in, vector<T>& v){
-    for (T& i : v) in >> i;
-    return in;
-}
-
-template<typename T = int> ostream& operator<<(ostream& out, const vector<T>& v){
-    for (const T& x : v)
-        out << x << ' ';
-    return out;
-}
-
-template<typename T = pair<int, int>> ostream& operator << (ostream& out, const vector<pair<int, int>>& v){
-    for(auto& [x, y] : v){
-        out << x << ' ' << y << nl;
+        iota(all(p), 0);
     }
-    return out;
-}
 
-void Start_Crushing() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-}
-
-struct station{
-    int dist, cost, t, freq, st;
-
-    station(){
-        dist = cost = t = freq = st = 0;
+    int get(int x){
+        return p[x] = x == p[x] ? x : get(p[x]);
     }
-    station(int dist, int cost, int t, int freq, int st){
-        this -> dist = dist;
-        this -> cost = cost;
-        this -> t = t;
-        this -> freq = freq;
-        this -> st = st;
+
+    void join(int u, int v){
+        u = get(u), v = get(v);
+
+        if(u == v)
+            return;
+
+        if(rank[u] == rank[v])
+            rank[u]++;
+
+        if(rank[u] > rank[v])
+            p[v] = u;
+        else
+            p[u] = v;
     }
 };
-
-//vector<vector<station>> adj;
-//vector<bool> vis;
-//vector<ll> cost, dist;
-int n, x;
-vector<vector<ll>> memo;
-vector<int> price, pages;
-
-ll dp(int idx, ll curr){
-    if(idx >= n){
-        return 0;
-    }
-
-    ll& ret = memo[idx][curr];
-    if(~ret)
-        return ret;
-
-    ret = dp(idx + 1, curr);
-    if(curr + price[idx] <= x)
-        updmax(ret, pages[idx] + dp(idx + 1, curr + price[idx]));
-
-    return ret;
-}
-
-void solve(){
-//    int n, m; cin >> n >> m;
-//
-//    adj.assign(n + 1, vector<station>());
-//    dist.assign(n + 1, inf);
-//    cost.assign(n + 1, inf);
-//    vis.assign(n + 1, false);
-//
-//    while(m--) {
-//        int u, v, t, c, f, s;
-//        cin >> u >> v >> t >> c >> f >> s;
-//        adj[u].emplace_back(v, c, t, f, s);
-//    }
-
-    cin >> n >> x;
-    price.resize(n), pages.resize(n);
-    memo.assign(n + 1, vector<ll>(x + 1, -1));
-    cin >> price >> pages;
-
-    cout << dp(0, 0);
-}
-
-int main(){
-    Start_Crushing();
-//    freopen("lis.in", "r", stdin);
-
-    int t = 1;
-//    /*Multiple test cases?*/ cin >> t;
-    while (t--) {
-        solve();
-        if(!t)
-            break;
-        cout << nl;
-    }
-
-//    for(int tc = 1; tc <= t; tc++){
-//        cout << "Case #" << tc << ": ";
-//        solve();
-//        if(tc != t)
-//            cout << nl;
-//    }
-
-    return 0;
-}
