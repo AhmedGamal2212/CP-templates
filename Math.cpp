62688b0d7a421b1bfd08623754f549ecd07444db
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2022-08-22 00:15:11 +0200

    Add Pisano Period

--- Math.cpp
+++ Math.cpp
@@ -227,4 +227,46 @@ xn = a + d(n−1)
 
 */
 
+// Pisano Period for Fibonacci numbers mod m O(m * m)
 
+long pisano(long m){
+    long prev = 0;
+    long curr = 1;
+    long res = 0;
+ 
+    for(int i = 0; i < m * m; i++){
+        long temp = 0;
+        temp = curr;
+        curr = (prev + curr) % m;
+        prev = temp;
+ 
+        if (prev == 0 && curr == 1)
+            res = i + 1;
+    }
+    return res;
+}
+ 
+// Calculate Fn mod m
+long fibonacciModulo(long n, long m){
+     
+    // Getting the period
+    long pisanoPeriod = pisano(m);
+ 
+    n = n % pisanoPeriod;
+ 
+    long prev = 0;
+    long curr = 1;
+ 
+    if (n == 0)
+        return 0;
+    else if (n == 1)
+        return 1;
+ 
+    for(int i = 0; i < n - 1; i++){
+        long temp = 0;
+        temp = curr;
+        curr = (prev + curr) % m;
+        prev = temp;
+    }
+    return curr % m;
+}
