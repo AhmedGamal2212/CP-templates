2c5b6d8f8cd564a6a7735d0e591b04ed3e61c40d
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2022-08-21 23:56:00 +0200

    Graph

--- Graph.cpp
+++ Graph.cpp
@@ -150,3 +150,400 @@ int main() {
 }
 
 
+// SCC: condensation
+
+vector<vector<int>> adj, adj_rev;
+vector<bool> used;
+vector<int> order, component;
+
+void dfs1(int v) {
+    used[v] = true;
+
+    for (auto u : adj[v])
+        if (!used[u])
+            dfs1(u);
+
+    order.push_back(v);
+}
+
+void dfs2(int v) {
+    used[v] = true;
+    component.push_back(v);
+
+    for (auto u : adj_rev[v])
+        if (!used[u])
+            dfs2(u);
+}
+
+int main() {
+    int n;
+    // ... read n ...
+
+    for (;;) {
+        int a, b;
+        // ... read next directed edge (a,b) ...
+        adj[a].push_back(b);
+        adj_rev[b].push_back(a);
+    }
+
+    used.assign(n, false);
+
+    for (int i = 0; i < n; i++)
+        if (!used[i])
+            dfs1(i);
+
+    used.assign(n, false);
+    reverse(order.begin(), order.end());
+
+    for (auto v : order)
+        if (!used[v]) {
+            dfs2 (v);
+
+            // ... processing next component ...
+
+            component.clear();
+        }
+}
+
+
+// Condensation graph construction
+
+// continuing from previous code
+
+vector<int> roots(n, 0);
+vector<int> root_nodes;
+vector<vector<int>> adj_scc(n);
+
+for (auto v : order)
+    if (!used[v]) {
+        dfs2(v);
+
+        int root = component.front();
+        for (auto u : component) roots[u] = root;
+        root_nodes.push_back(root);
+
+        component.clear();
+    }
+
+
+for (int v = 0; v < n; v++)
+    for (auto u : adj[v]) {
+        int root_v = roots[v],
+            root_u = roots[u];
+
+        if (root_u != root_v)
+            adj_scc[root_v].push_back(root_u);
+    }
+
+
+// SCC: Tarjan
+
+void tarjan(int src){
+    dfs_num[src] = dfs_low[src] = counter++;
+
+    dfs_stack.push_back(src);
+    in_stack[src] = true;
+
+    for(auto& nxt : adj[src]){
+        if(not dfs_num[nxt])
+            tarjan(nxt);
+
+        if(in_stack[nxt])
+            dfs_low[src] = min(dfs_low[src], dfs_low[nxt]);
+    }
+
+
+    // this is the the root of the SCC
+
+    if(dfs_low[src] == dfs_num[src]){
+        while(true){
+            int curr = dfs_stack.back();
+            dfs_stack.pop_back();
+            in_stack[curr] = false;
+
+            if(curr == src)
+                break;
+        }
+    }
+}
+
+
+// MST: Kruskal 
+
+struct Edge {
+    int u, v, weight;
+    bool operator<(Edge const& other) {
+        return weight < other.weight;
+    }
+};
+
+int n;
+vector<Edge> edges;
+
+int cost = 0;
+vector<int> tree_id(n);
+vector<Edge> result;
+for (int i = 0; i < n; i++)
+    tree_id[i] = i;
+
+sort(edges.begin(), edges.end());
+
+for (Edge e : edges) {
+    if (tree_id[e.u] != tree_id[e.v]) {
+        cost += e.weight;
+        result.push_back(e);
+
+        int old_id = tree_id[e.u], new_id = tree_id[e.v];
+        for (int i = 0; i < n; i++) {
+            if (tree_id[i] == old_id)
+                tree_id[i] = new_id;
+        }
+    }
+}
+
+
+// MST: Kruskal (DSU)
+
+vector<int> parent, rank;
+
+void make_set(int v) {
+    parent[v] = v;
+    rank[v] = 0;
+}
+
+int find_set(int v) {
+    if (v == parent[v])
+        return v;
+    return parent[v] = find_set(parent[v]);
+}
+
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b) {
+        if (rank[a] < rank[b])
+            swap(a, b);
+        parent[b] = a;
+        if (rank[a] == rank[b])
+            rank[a]++;
+    }
+}
+
+struct Edge {
+    int u, v, weight;
+    bool operator<(Edge const& other) {
+        return weight < other.weight;
+    }
+};
+
+int n;
+vector<Edge> edges;
+
+int cost = 0;
+vector<Edge> result;
+parent.resize(n);
+rank.resize(n);
+for (int i = 0; i < n; i++)
+    make_set(i);
+
+sort(edges.begin(), edges.end());
+
+for (Edge e : edges) {
+    if (find_set(e.u) != find_set(e.v)) {
+        cost += e.weight;
+        result.push_back(e);
+        union_sets(e.u, e.v);
+    }
+}
+
+
+
+// Floyd-Warshall
+
+
+for (int k = 0; k < n; ++k) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (d[i][k] < INF && d[k][j] < INF)
+                d[i][j] = min(d[i][j], d[i][k] + d[k][j]); 
+        }
+    }
+}
+
+// Modification in Floyd Warshall Algorithm (path contstruction)
+
+#define MAXN 100
+
+// Infinite value for array
+
+const int INF = 1e7;
+ 
+int dis[MAXN][MAXN];
+int Next[MAXN][MAXN];
+ 
+// Initializing the distance and
+// Next array
+
+void initialise(int V, vector<vector<int> >& graph){
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            dis[i][j] = graph[i][j];
+ 
+            // No edge between node
+            // i and j
+            
+            if(graph[i][j] == INF)
+                Next[i][j] = -1;
+            else
+                Next[i][j] = j;
+        }
+    }
+}
+ 
+// Function construct the shortest
+// path between u and v
+
+vector<int> constructPath(int u, int v){
+    // If there's no path between
+    // node u and v, simply return
+    // an empty array
+    
+    if (Next[u][v] == -1)
+        return {};
+ 
+    // Storing the path in a vector
+    
+    vector<int> path = {u};
+    while (u != v) {
+        u = Next[u][v];
+        path.push_back(u);
+    }
+    return path;
+}
+ 
+// Standard Floyd Warshall Algorithm
+// with little modification Now if we find
+// that dis[i][j] > dis[i][k] + dis[k][j]
+// then we modify next[i][j] = next[i][k]
+
+void floydWarshall(int V){
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+ 
+                // We cannot travel through
+                // edge that doesn't exist
+
+                if (dis[i][k] == INF || dis[k][j] == INF)
+                    continue;
+ 
+                if (dis[i][j] > dis[i][k] + dis[k][j]){
+                    dis[i][j] = dis[i][k] + dis[k][j];
+                    Next[i][j] = Next[i][k];
+                }
+            }
+        }
+    }
+}
+ 
+// Print the shortest path
+
+void printPath(vector<int>& path){
+    int n = path.size();
+    for (int i = 0; i < n - 1; i++)
+        cout << path[i] << " -> ";
+    cout << path[n - 1] << endl;
+}
+
+
+// LCA: binary lifting 
+
+template < typename T = int > struct LCA {
+    
+    struct Edge {
+
+        ll v, w;
+
+        Edge(ll V = 0, ll W = 0) : v(V), w(W) {}
+
+        bool operator < (const Edge &rhs) const {
+            return w < rhs.w;
+        }
+
+    };
+
+    int N, LOG;
+    vector < vector < T > > anc, cost;
+    vector < vector < Edge > > adj;
+    vector < int > dep;
+    
+    LCA(int n){
+        N = n + 10, LOG = 0;
+        while((1 << LOG) <= n) LOG++;
+        dep = vector < int > (N);
+        adj = vector < vector < Edge > > (N);
+        anc = cost = vector < vector < T > > (N, vector < T > (LOG));
+    }
+    
+    void add_edge(int u, int v, T w){
+        adj[u].push_back(Edge(v, w));
+        adj[v].push_back(Edge(u, w));
+    }
+
+    void build_adj(int edges){
+        for(int i = 0, u, v, w; i < edges && cin >> u >> v >> w; i++)
+            add_edge(u, v, w);
+    }
+
+    T operation(T a, T b){
+        return a + b;
+    }
+    
+    void dfs(int u, int p = 0){
+        for(auto& [v, w] : adj[u]){
+            if(v == p) continue;
+            dep[v] = dep[u] + 1, anc[v][0] = u, cost[v][0] = w;
+            for(int bit = 1; bit < LOG; bit++){
+                anc[v][bit] = anc[anc[v][bit - 1]][bit - 1];
+                cost[v][bit] = operation(cost[v][bit - 1], cost[anc[v][bit - 1]][bit - 1]);
+            }
+            dfs(v, u);
+        }
+    }
+    
+    int kth_ancestor(int u, int k){
+        if(dep[u] < k) 
+            return -1;
+        for(int bit = LOG - 1; bit >= 0; bit--)
+            if(k & (1 << bit))
+                u = anc[u][bit];
+        return u;
+    }
+    
+    int get_lca(int u, int v){
+        if(dep[u] < dep[v])
+            swap(u, v);
+        u = kth_ancestor(u, dep[u] - dep[v]);
+        if(u == v)
+            return u;
+        for(int bit = LOG - 1; bit >= 0; bit--)
+            if(anc[u][bit] != anc[v][bit])
+                u = anc[u][bit], v = anc[v][bit];
+        return anc[u][0];
+    }
+    
+    T get_cost(int u, int dist){
+        if(dep[u] < dist) return -1;
+        T ans = 0;
+        for(int bit = 0; bit < LOG; bit++)
+            if(dist & (1 << bit))
+                ans = operation(ans, cost[u][bit]), u = anc[u][bit];
+        return ans;
+    }
+    
+    T query(int u, int v){
+        int lca = get_lca(u, v);
+        return operation(get_cost(u, dep[u] - dep[lca]), get_cost(v, dep[v] - dep[lca]));
+    }
+
+};
