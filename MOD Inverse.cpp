ee37429a292474c60ba334703ade0c2962276fba
ahmedgamal2212 <ahmedgemy2212@gmail.com>
2023-02-17 17:36:04 +0200

    MODINV

--- MOD Inverse.cpp
+++ MOD Inverse.cpp
@@ -1,10 +1,10 @@
-struct MOD_Inverse {
-
+template<typename T = ll> struct MODINV {
+ 
     ll n;
     int mod;
     vector<ll> fact, inv;
-
-    ll fast_power(ll b, ll e){
+ 
+    ll fast_power(ll b, ll e) {
         ll power = 1;
         while(e){
             if(e & 1) power = modulo(power, b, mod);
@@ -12,19 +12,51 @@ struct MOD_Inverse {
         }
         return power % mod;
     }
-
-    ll Inverse(ll x){
+ 
+    ll inverse(ll x) {
         return fast_power(x, mod - 2) % mod;
     }
-
-    MOD_Inverse(ll n, int mod){
-        this -> n = n;
-        this -> mod = mod;
+ 
+    MODINV(){
+        n = mod = 0;
+    }
+ 
+    MODINV(ll n, int mod){
+        assign(n, mod);
+    }
+ 
+    void assign(ll n_, int mod_){
+        this -> n = n_;
+        this -> mod = mod_;
         fact.assign(n + 10, 1), inv.resize(n + 10, 1);
         for(ll i = 1; i <= n; i++){
             fact[i] = modulo(fact[i - 1], i, mod);
-            inv[i] = Inverse(fact[i]);
+            inv[i] = inverse(fact[i]);
         }
     }
-
+ 
+    void add(T& a, T b){
+        a += b;
+        if(a >= mod)
+            a -= mod;
+    }
+ 
+    void mul(T& a, T b){
+        ll res = 0;
+        while(b){
+            if(b & 1) {
+                add(res, a);
+            }
+            add(a, a);
+            b >>= 1;
+        }
+        a = res;
+    }
+ 
+    T nCr(T n_, T r){
+        T res = 1;
+        for(auto& i : {fact[n_], inv[r], inv[n_ - r]})
+            mul(res, i);
+        return res;
+    }
 };
