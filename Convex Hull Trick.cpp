d21b4743cedfd17b32e2616f614653ce8a08431f
ahmedgamal2212 <ahmedgemy2212@gmail.com>
2023-02-22 15:36:36 +0200

    Add Default Constructor to Convex Hull Trick

--- Convex Hull Trick.cpp
+++ Convex Hull Trick.cpp
@@ -1,18 +1,18 @@
 struct Line {
     mutable ll m, c, p;
 
-    bool operator<(const Line &o) const { 
-        return m < o.m; 
+    bool operator<(const Line &o) const {
+        return m < o.m;
     }
 
-    bool operator<(ll x) const { 
-        return p < x; 
+    bool operator<(ll x) const {
+        return p < x;
     }
 };
 
 class LineContainer : multiset<Line, less<>> {
 private:
-    const int mode; // maximum mode = 1, minimum mode = -1
+   int mode; // maximum mode = 1, minimum mode = -1
 
     // TODO: (for doubles, use inf = 1/.0, div(a,b) = a/b)
 //    static const ll inf = LLONG_MAX;
@@ -22,31 +22,43 @@ private:
     }
 
     bool isect(iterator x, iterator y){
-        if (y == end()) 
-            return x->p = llmax, false;
-        if (x -> m == y -> m) 
+        if (y == end())
+            return x -> p = llmax, false;
+        if (x -> m == y -> m)
             x -> p = x -> c > y -> c ? llmax : llmin;
-        else 
+        else
             x -> p = div(y -> c - x -> c, x -> m - y -> m);
-        return x->p >= y->p;
+        return x -> p >= y -> p;
     }
 
 public:
     // maximum mode = 1, minimum mode = -1
+    // default mode = minimum
+    LineContainer() : mode(-1) {}
+
     explicit LineContainer(const int md) : mode(md) {}
+
+    void set_mode(const int md){
+        mode = md;
+    }
 
     void add(ll m, ll c){
         m *= mode, c *= mode;
         auto z = insert({m, c, 0}), y = z++, x = y;
         while(isect(y, z))
             z = erase(z);
-        if(x != begin() and isect(--x, y)) 
+        if(x != begin() and isect(--x, y))
             isect(x, y = erase(y));
         while((y = x) != begin() and (--x) -> p >= y -> p)
             isect(x, erase(y));
     }
 
     ll query(ll x){
+        // default value for maximum mode = -inf, minimum mode = +inf (if you want to change it)
+        // if(empty()){
+        //     return llmax;
+        // }
+
         assert(not empty());
         auto l = *lower_bound(x);
         return mode * (l.m * x + l.c);
