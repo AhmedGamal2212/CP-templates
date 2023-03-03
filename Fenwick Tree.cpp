8e15c4f8e5c0cb760d8d95f43cc116a853f96753
ahmedgamal2212 <ahmedgemy2212@gmail.com>
2023-03-03 16:20:31 +0200

    Update Fenwick Tree

--- Fenwick Tree.cpp
+++ Fenwick Tree.cpp
@@ -1,55 +1,56 @@
 template<typename T>
-struct fenwick_tree{
+struct fenwick_tree {
     int n{};
     vector<T> bit;
+    const T DEFAULT = 0;
 
     fenwick_tree() = default;
 
-    void init(int n_){
-        this -> n = n_;
-        bit.assign(n_ + 1, 0);
+    void init(int n_) {
+        this->n = n_;
+        bit.assign(n_ + 1, DEFAULT);
     }
 
-    explicit fenwick_tree(int n){
+    explicit fenwick_tree(int n) {
         init(n);
     }
 
-    explicit fenwick_tree(vector<T>& v) : fenwick_tree(int(v.size())){
+    explicit fenwick_tree(vector<T> &v) : fenwick_tree(int(v.size())) {
         int idx = 0;
-        while(idx < n){
+        while (idx < n) {
             add(idx + 1, v[idx]);
             idx++;
         }
     }
 
-    void add(int idx, int value){
-        while(idx <= n){
+    void add(int idx, T value) {
+        while (idx <= n) {
             bit[idx] += value;
             idx += idx & -idx;
         }
     }
 
-    T sum(int idx){
+    T sum(int idx) {
         T ret = 0;
-        while(idx){
+        while (idx) {
             ret += bit[idx];
             idx ^= idx & -idx;
         }
         return ret;
     }
 
-    ll sum(int l, int r){
-        if(l > r)
+    T sum(int l, int r) {
+        if (l > r)
             return 0;
         return sum(r) - sum(l - 1);
     }
 
-    void update_range(int l, int r, int value){
+    void update_range(int l, int r, T value) {
         add(l, value);
         add(r + 1, -value);
     }
 
-    int size(){
-        return this -> n;
+    int size() {
+        return this->n;
     }
 };
