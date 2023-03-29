addd277b7362794523c5fe1197c0adda056f398a
ahmedgamal2212 <ahmedgemy2212@gmail.com>
2023-03-29 03:34:21 +0200

    Update MOD Inverse

--- MOD Inverse.cpp
+++ MOD Inverse.cpp
@@ -1,49 +1,52 @@
 template<typename T = ll> struct MODINV {
- 
-    ll n;
-    int mod;
+
+    ll n{};
+    int mod{};
     vector<ll> fact, inv;
- 
-    ll fast_power(ll b, ll e) {
-        ll power = 1;
-        while(e){
-            if(e & 1) power = modulo(power, b, mod);
-            e >>= 1, b = modulo(b, b, mod);
+
+    ll bin_exp(ll b, ll e) {
+        ll res = 1;
+        while(e) {
+            if(e & 1) {
+                res = modulo(res, b, mod);
+            }
+            b = modulo(b, b, mod);
+            e >>= 1;
         }
-        return power % mod;
+        return res;
     }
- 
+
     ll inverse(ll x) {
-        return fast_power(x, mod - 2) % mod;
+        return bin_exp(x, mod - 2) % mod;
     }
- 
-    MODINV(){
+
+    MODINV() {
         n = mod = 0;
     }
- 
-    MODINV(ll n, int mod){
+
+    MODINV(ll n, int mod) {
         assign(n, mod);
     }
- 
-    void assign(ll n_, int mod_){
-        this -> n = n_;
-        this -> mod = mod_;
+
+    void assign(ll n_, int mod_) {
+        this->n = n_;
+        this->mod = mod_;
         fact.assign(n + 10, 1), inv.resize(n + 10, 1);
-        for(ll i = 1; i <= n; i++){
+        for(ll i = 1; i <= n; i++) {
             fact[i] = modulo(fact[i - 1], i, mod);
             inv[i] = inverse(fact[i]);
         }
     }
- 
-    void add(T& a, T b){
+
+    void add(T& a, T b) {
         a += b;
         if(a >= mod)
             a -= mod;
     }
- 
-    void mul(T& a, T b){
+
+    void mul(T& a, T b) {
         ll res = 0;
-        while(b){
+        while(b) {
             if(b & 1) {
                 add(res, a);
             }
@@ -52,12 +55,12 @@ template<typename T = ll> struct MODINV {
         }
         a = res;
     }
- 
-    T nCr(T n_, T r){
+
+    T nCr(T n_, T r) {
         return modulo(nPr(n_, r), inv[r], mod);
     }
 
-    T nPr(T n_, T r){
+    T nPr(T n_, T r) {
         T res = 1;
         for(auto& i : {fact[n_], inv[n_ - r]})
             mul(res, i);
