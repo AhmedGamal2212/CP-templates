55a494a1ad4e889e64beb6c2fa2bb16fc54c836c
ahmedgamal2212 <ahmedgemy2212@gmail.com>
2023-05-21 17:17:29 +0300

    Edit Math Struct

--- Math.cpp
+++ Math.cpp
@@ -1,40 +1,40 @@
-struct Math {
-    template<typename T> static ll bin_exp(T b, T e, int mod) {
+namespace Math {
+    template<typename T> static ll bin_exp(T b, T e, int mod = MOD) {
         ll res = 1;
         while(e) {
             if(e & 1)
                 res = modulo(res, b, mod);
- 
+
             b = modulo(b, b, mod);
             e >>= 1;
         }
         return res;
     }
- 
-    template<typename T, typename E> static void sub(T& a, E b, int mod) {
+
+    template<typename T, typename E> static void sub(T& a, E b, int mod = MOD) {
         a -= b;
         if(a < 0)
             a += mod;
     }
- 
-    template<typename T, typename E> static void add(T& a, E b, int mod) {
+
+    template<typename T, typename E> static void add(T& a, E b, int mod = MOD) {
         a += b;
         if(a >= mod)
             a -= mod;
     }
- 
-    template<typename T> static T inv(T x, T mod) {
+
+    template<typename T> static T inv(T x, T mod = MOD) {
         x %= mod;
         if(x < 0)
             x += mod;
- 
+
         T b = mod, ret = 0, c = 1;
         while(x) {
             int tmp = b / x;
- 
+
             b -= tmp * x;
             swap(x, b);
- 
+
             ret -= tmp * c;
             swap(ret, c);
         }
@@ -42,8 +42,8 @@ struct Math {
             ret += mod;
         return ret;
     }
- 
-    template<typename T> static void mul(T& a, T b, T mod) {
+
+    template<typename T> static void mul(T& a, T b, T mod = MOD) {
         ll res = 0;
         while(b) {
             if(b & 1) {
@@ -53,13 +53,5 @@ struct Math {
             b >>= 1;
         }
         a = res;
-    }
- 
-    template<typename T> static ll mul(vector<T> v, T mod){
-        ll res = 1;
-        for(auto& i : v){
-            mul(res, i, mod);
-        }
-        return res;
     }
 };
