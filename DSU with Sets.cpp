79a8895cda46dd1136e2d638bef965ccdf3e62b4
ahmedgamal2212 <ahmedgemy2212@gmail.com>
2023-05-30 12:46:41 +0300

    Create DSU with Sets

---
+++ DSU with Sets.cpp
@@ -0,0 +1,51 @@
+struct DSUWithSets {
+    vector<int> group_size, par, roots, tail, nxt, pos;
+
+    explicit DSUWithSets(int size) {
+        par.assign(size + 1, -1);
+        group_size = vector<int>(size + 1, 1);
+        roots = pos = tail = vector<int>(size + 1);
+        nxt.assign(size + 1, -1);
+
+        iota(all(roots), 0);
+        iota(all(pos), 0);
+        iota(all(tail), 0);
+    }
+
+    int find(int u) {
+        if(par[u] == -1) {
+            return u;
+        }
+        return par[u] = find(par[u]);
+    }
+
+    bool join(int x, int y) {
+        x = find(x), y = find(y);
+        if(x == y) {
+            return false;
+        }
+        if(group_size[x] > group_size[y]) {
+            swap(x, y);
+        }
+        par[x] = y;
+        group_size[y] += group_size[x];
+
+        int p = pos[x];
+        roots[p] = roots.back();
+        pos[roots[p]] = p;
+        roots.pop_back();
+
+        nxt[tail[y]] = x;
+        tail[y] = tail[x];
+
+        return true;
+    }
+
+    void print() {
+        for(auto& x : roots) {
+            for(int y = x; ~y; y = nxt[y]) {
+                cout << y << " \n"[!~nxt[y]];
+            }
+        }
+    }
+};
