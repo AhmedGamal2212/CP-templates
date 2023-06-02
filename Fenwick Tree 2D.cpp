1b6d0edf127b7d44d03916b1d7c06939fe4d1c27
ahmedgamal2212 <ahmedgemy2212@gmail.com>
2023-06-02 19:45:03 +0300

    Update Fenwick Tree 2D

--- Fenwick Tree 2D.cpp
+++ Fenwick Tree 2D.cpp
@@ -1,27 +1,28 @@
-struct fenwick_tree_2d{
-    int n, m;
-    vector<vector<int>> bit;
+template<typename T = int, typename E = int> struct fenwick_tree_2d {
+    int n {}, m {};
+    vector<vector<T>> bit;
 
-    fenwick_tree_2d(int n, int m){
-        this -> n = n;
-        this -> m = m;
-        bit.assign(n + 1, vector<int>(m + 1));
+    fenwick_tree_2d(int n, int m) {
+        this->n = n;
+        this->m = m;
+        bit.assign(n + 1, vector<T>(m + 1));
     }
 
-    fenwick_tree_2d(vector<vector<int>>& v) : fenwick_tree_2d(v.size(), v.front().size()){
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                add(i + 1, j + 1, v[i][j]);
+    explicit fenwick_tree_2d(vector<vector<E>>& v): fenwick_tree_2d(v.size(), v.front().size()) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                add(i, j, v[i][j]);
             }
         }
     }
 
-    int sum(int x, int y){
-        int ret = 0;
-        while(x){
+    T sum(int x, int y) {
+        x++, y++;
+        T ret = 0;
+        while(x) {
             int idx = y;
-            while(idx){
-                ret += bit[x][idx];
+            while(idx) {
+                ret += bit[x - 1][idx - 1];
                 idx ^= idx & -idx;
             }
             x ^= x & -x;
@@ -29,11 +30,12 @@ struct fenwick_tree_2d{
         return ret;
     }
 
-    void add(int x, int y, int value){
-        while(x <= n){
+    void add(int x, int y, int value) {
+        x++, y++;
+        while(x <= n) {
             int idx = y;
-            while(idx <= m){
-                bit[x][idx] += value;
+            while(idx <= m) {
+                bit[x - 1][idx - 1] += value;
                 idx += idx & -idx;
             }
             x += x & -x;
